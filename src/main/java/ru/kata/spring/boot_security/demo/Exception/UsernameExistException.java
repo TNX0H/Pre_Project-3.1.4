65cc52a05df82c6fe8b2ead1d52d79f68c1cadc9
TNX0H <77393041+TNX0H@users.noreply.github.com>
2022-09-21 23:43:08 +0300

    Delete src directory

--- src/main/java/ru/kata/spring/boot_security/demo/Exception/UsernameExistException.java
+++
@@ -1,9 +0,0 @@
-package ru.kata.spring.boot_security.demo.Exception;
-
-import org.springframework.dao.DataIntegrityViolationException;
-
-public class UsernameExistException extends DataIntegrityViolationException {
-    public UsernameExistException(String msg) {
-        super(msg);
-    }
-}
